bef3c27ac838a84c8ed3921851feeaf33a41cfcb
juan carlos macias <juancmaciassalvador@gmail.com>
2023-07-20 18:44:07 +0200

    error

--- detail.js
+++ detail.js
@@ -1,4 +1,11 @@
-const getData = async(id) => {
+const getData = async() => {
+	let params = new URLSearchParams(location.search);
+	var id = params.get('id');
+     //Se realiza la división de la URL
+     //var split = actual.split("/");
+     //Se obtiene el ultimo valor de la URL
+     //var id = split[split.length-1];
+     console.log(id);
 	let response = await fetch(`http://127.0.0.1:8000/ofer/${id}`);
     let data = await response.json();
 	console.log(data);
@@ -9,28 +16,28 @@ const printJobDetail = (data) => {
 	const jobDetail = document.getElementById('jobDetail');
 	let card = document.createElement("div");
 	let cardInfo = '';
-	for (let i = 0; i < data.length; i++) {
+
 		
 			card.classList.add("detailCard");
 			jobDetail.appendChild(card);
 			
 			cardInfo +=
 				`<div class="img-container">
-					<img src="http://127.0.0.1:8000/${data[i].image}" alt=${data[i].companyName}>
+					<img src="http://127.0.0.1:8000/${data.image}" alt=${data.companyName}>
 				</div>
 				<div class="info">
-					<a href="detail.html?id=${data[i].id}" title="${data[i].positionTitle}" target="_top">
-						<span class="positionTitle">${data[i].positionTitle}</span>
-					</a>
-					<h3 class="companyName">${data[i].companyName}</h3>
-                    <p class="companyDescription">${data[i].description}</p>
-                    <p class="positionDescription">${data[i].positionDescription}</p>
-                    <p class="requirements">${data[i].requirements}</p>
-                    <p class="salary">${data[i].salary}</p>
-					<small class="employmentMode">Modalidad: ${data[i].employmentMode}<span></span></small>
+					
+						<span class="positionTitle">${data.positionTitle}</span>
+
+					<h3 class="companyName">${data.companyName}</h3>
+                    <p class="companyDescription">${data.description}</p>
+                    <p class="positionDescription">${data.positionDescription}</p>
+                    <p class="requirements">${data.requirements}</p>
+                    <p class="salary">${data.salary}</p>
+					<small class="employmentMode">Modalidad: ${data.employmentMode}<span></span></small>
 				</div>`;
-		console.log("id "+ data[i].id);
-	  }
+		console.log("id "+ data.id);
+
 	
 	
 	
@@ -38,5 +45,5 @@ const printJobDetail = (data) => {
 
 }
 
-getData(6);
+getData();
 
