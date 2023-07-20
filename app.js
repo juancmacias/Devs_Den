8cb3c3e0560b2a0c3eeaeb6acf844f8de1a4fd75
juan carlos macias <juancmaciassalvador@gmail.com>
2023-07-20 18:05:26 +0200

    buscador

--- app.js
+++ app.js
@@ -15,15 +15,17 @@ const printPokemon = (data) => {
 			poke_container.appendChild(card);
 			
 			cardInfo +=
-				`<div class="img-container">
-					<img src="http://127.0.0.1:8000/${data[i].image}" alt=${data[i].companyName}>
-				</div>
-				<div class="info">
-					<a href="detail.html?id=${data[i].id}" title="${data[i].positionTitle}" target="_top">
-						<span class="number">${data[i].positionTitle}</span>
-					</a>
-					<h3 class="name">${data[i].companyName}</h3>
-					<small class="type">Modalidad: ${data[i].employmentMode}<span></span></small>
+				`<div class="buscar">
+					<div class="img-container">
+						<img src="http://127.0.0.1:8000/${data[i].image}" alt=${data[i].companyName}>
+					</div>
+					<div class="info">
+						<a href="detail.html?id=${data[i].id}" title="${data[i].positionTitle}" target="_top">
+							<span class="number">${data[i].positionTitle}</span>
+						</a>
+						<h3 class="name">${data[i].companyName}</h3>
+						<small class="type">Modalidad: ${data[i].employmentMode}<span></span></small>
+					</div>
 				</div>`;
 		console.log("id "+ data[i].id);
 	  }
@@ -35,3 +37,20 @@ const printPokemon = (data) => {
 }
 
 getData(6);
+
+function myFunction() {
+	var input = document.getElementById("Search");
+	var filter = input.value.toLowerCase();
+	var nodes = document.getElementsByClassName('buscar');
+  
+	for (i = 0; i < nodes.length; i++) {
+
+	  if (nodes[i].innerText.toLowerCase().includes(filter)) {
+		nodes[i].style.display = "block";
+		
+	  } else {
+		nodes[i].style.display = "none";
+		console.log("node "+ nodes[i])
+	  }
+	}
+  }
