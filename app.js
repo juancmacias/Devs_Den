261aa8969e3f1cfb5d519d96f97608f00b502b10
juan carlos macias <juancmaciassalvador@gmail.com>
2023-07-20 15:42:15 +0200

    fech

--- app.js
+++ app.js
@@ -1,32 +1,37 @@
 const getData = async(id) => {
-	let response = await fetch(`http://127.0.0.1:8000/ofer/${id}`);
+	let response = await fetch(`http://127.0.0.1:8000/ofer/jobpositions`);
     let data = await response.json();
 	console.log(data);
 	printPokemon(data);
 }
 
 const printPokemon = (data) => {
+	const poke_container = document.getElementById('poke-container');
 	let card = document.createElement("div");
-	card.classList.add("pokemon");
-	poke_container.appendChild(card);
+	let cardInfo = '';
+	for (let i = 0; i < data.length; i++) {
+		
+			card.classList.add("pokemon");
+			poke_container.appendChild(card);
+			
+			cardInfo +=
+				`<div class="img-container">
+					<img src="http://127.0.0.1:8000/${data[i].image}" alt=${data[i].companyName}>
+				</div>
+				<div class="info">
+					<a href="detail.html?id=${data[i].id}" title="${data[i].positionTitle}" target="_top">
+						<span class="number">${data[i].positionTitle}</span>
+					</a>
+					<h3 class="name">${data[i].companyName}</h3>
+					<small class="type">Modalidad: ${data[i].employmentMode}<span></span></small>
+				</div>`;
+		console.log("id "+ data[i].id);
+	  }
 	
-	let type = data.types[0].type.name;
-	let cardBackground = colors[type];
-	card.style.backgroundColor = cardBackground
 	
-
-
-	let cardInfo =
-		`<div class="img-container">
-			<img src= "http://127.0.0.1:8000/${data.image}" alt=${data.companyName}>
-		</div>
-		<div class="info">
-            <h3 class="name">${data.companyName}</h3>
-			<span class="number">${data.positionTitle}</span>
-			<small class="type">Modalidad: ${data.employmentMode}<span></span></small>
-		</div>`;
 	
 	card.innerHTML = cardInfo;
+
 }
 
-pokeIterator();
+getData(6);
