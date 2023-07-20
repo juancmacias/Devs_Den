a4111d9318039aa656d9bc7aa499be93950d562f
Hafdala Mehdi Sidi <hafdalamehdisidi@gmail.com>
2023-07-20 22:46:36 +0200

    Adjust the contact form, I have touched up the styles and I have given it functionality with js and jquery.

--- login.js
+++ login.js
@@ -1,35 +1,32 @@
 $(document).ready(function () {
+    $(".contenedor-formularios")
+        .find("input, textarea")
+        .on("keyup blur focus", function (e) {
+            var $this = $(this),
+                label = $this.prev("label");
 
-    $(".contenedor-formularios").find("input, textarea").on("keyup blur focus", function (e) {
-
-        var $this = $(this),
-            label = $this.prev("label");
-
-        if (e.type === "keyup") {
-            if ($this.val() === "") {
-                label.removeClass("active highlight");
-            } else {
-                label.addClass("active highlight");
+            if (e.type === "keyup") {
+                if ($this.val() === "") {
+                    label.removeClass("active highlight");
+                } else {
+                    label.addClass("active highlight");
+                }
+            } else if (e.type === "blur") {
+                if ($this.val() === "") {
+                    label.removeClass("active highlight");
+                } else {
+                    label.removeClass("highlight");
+                }
+            } else if (e.type === "focus") {
+                if ($this.val() === "") {
+                    label.removeClass("highlight");
+                } else if ($this.val() !== "") {
+                    label.addClass("highlight");
+                }
             }
-        } else if (e.type === "blur") {
-            if ($this.val() === "") {
-                label.removeClass("active highlight");
-            } else {
-                label.removeClass("highlight");
-            }
-        } else if (e.type === "focus") {
-            if ($this.val() === "") {
-                label.removeClass("highlight");
-            }
-            else if ($this.val() !== "") {
-                label.addClass("highlight");
-            }
-        }
-
-    });
+        });
 
     $(".tab a").on("click", function (e) {
-
         e.preventDefault();
 
         $(this).parent().addClass("active");
@@ -40,7 +37,5 @@ $(document).ready(function () {
         $(".contenido-tab > div").not(target).hide();
 
         $(target).fadeIn(600);
-
     });
-
 });
